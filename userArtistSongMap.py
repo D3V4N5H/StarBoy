425803542ad62e053110398b8d9fd56baa5f9326
Devansh Trivedi <devanshrtrivedi@gmail.com>
2018-10-03 00:02:45 +0530

    Day 14 update
    Made a function to handle APIs better
    Combined approach: Finding Best songs OF Best artists

--- userArtistSongMap.py
+++ userArtistSongMap.py
@@ -29,39 +29,48 @@ def makeNodesFromAPI(driver, uName, aName, sName):
 
 graph = Graph(uri, user, password)
 graph.clearGraph(graph._driver)
-topArtistsIndiaURL = "https://api.musixmatch.com/ws/1.1/chart.artists.get?format=jsonp&callback=callback&country=in&apikey="+apikey
-topArtistsIndiaResponse=requests.get(topArtistsIndiaURL)
-stringWithoutCallbackHead=topArtistsIndiaResponse.text.replace( "callback(" , "")
-stringWtihoutCallbackHeadAndTail=stringWithoutCallbackHead.replace( ");" , "" )
-topArtistsIndiaData=json.loads(stringWtihoutCallbackHeadAndTail)
+
+
+def callAPI(url):
+    response = requests.get( url + "&apikey=" +apikey ).text
+    callbackToJson = response.replace( "callback(" , "").replace( ");" , "" )
+    return json.loads(callbackToJson)
+
+
+topArtistsIndiaURL = "https://api.musixmatch.com/ws/1.1/chart.artists.get?format=jsonp&callback=callback&country=in"
+topArtistsIndiaData = callAPI(topTracksIndiaURL)
 # topArtistsIndiaData["message"]["body"]["artist_list"][i]["artist"]["artist_name"]
-
 listOfArtists=[]
-
 for i in range(len(topArtistsIndiaData["message"]["body"]["artist_list"])):
     listOfArtists.append(topArtistsIndiaData["message"]["body"]["artist_list"][i]["artist"]["artist_name"])
 
-topTracksIndiaURL = "https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&country=in&apikey="+apikey
-topTracksIndiaResponse=requests.get(topTracksIndiaURL)
-stringWithoutCallbackHead=topTracksIndiaResponse.text.replace( "callback(" , "")
-stringWtihoutCallbackHeadAndTail=stringWithoutCallbackHead.replace( ");" , "" )
-topTracksIndiaData=json.loads(stringWtihoutCallbackHeadAndTail)
 
+topTracksIndiaURL = "https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=callback&country=in"
+topTracksIndiaData = callAPI(topTracksIndiaURL)
 listOfTracks=[]
-
 for i in range(len(topTracksIndiaData["message"]["body"]["track_list"])):
     listOfTracks.append(topTracksIndiaData["message"]["body"]["track_list"][i]["track"]["track_name"])
 
+
 def getTrackListOfArtist(name):
     searchTracksOfArtistURL = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_artist="+name+"&quorum_factor=1&apikey="+apikey
-    searchTracksOfArtistResponse=requests.get(searchTracksOfArtistURL)
-    stringWithoutCallbackHead=searchTracksOfArtistResponse.text.replace( "callback(" , "")
-    stringWtihoutCallbackHeadAndTail=stringWithoutCallbackHead.replace( ");" , "" )
-    searchTracksOfArtistData=json.loads(stringWtihoutCallbackHeadAndTail)
+    searchTracksOfArtistData = callAPI(searchTracksOfArtistURL)
     songsOfArtist=[]
     for i in range(len(searchTracksOfArtistData["message"]["body"]["track_list"])):
         songsOfArtist.append(searchTracksOfArtistData["message"]["body"]["track_list"][i]["track"]["track_name"])
     return songsOfArtist
+
+
+for artist in listOfArtists:
+    print(artist + ":")
+    print()
+    count=0
+    for song in getTrackListOfArtist(artist):
+        count = count + 1
+        print( count, end=". " )
+        print( song )
+    print()
+    print()
 
 # genre= searchTracksOfArtistData["message"]["body"]["track_list"][i]["track"]["primary_genres"]["music_genre_list"][0]["music_genre"]["music_genre_name"]
 userNameCount=0
