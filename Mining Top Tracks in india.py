c4667d792249211593b6eb52e816e72f125eb72a
Devansh Trivedi <devanshrtrivedi@gmail.com>
2018-10-13 23:46:33 +0530

    Day 25 update
    Mining from Lyrics of Songs

--- Mining Top Tracks in india.py
+++ Mining Top Tracks in india.py
@@ -18,9 +18,11 @@ class Graph(object):
     def createNodes(tx, word, nextWord):
         result = tx.run('''
             MERGE (lx:lyrics{word:$word})
+            	ON CREATE SET lx.word = $word
                 ON CREATE SET lx.count = 1
                 ON MATCH SET lx.count = lx.count + 1
             MERGE (mx:lyrics{word:$nextWord})
+                ON CREATE SET mx.word = $word
                 ON CREATE SET mx.count = 1
                 ON MATCH SET mx.count = mx.count + 1
             MERGE (lx)-[r:next]->(mx)
@@ -103,8 +105,11 @@ removeLyricsLabel='''
 	RETURN node
 	'''
 
-
+#Mining
 for key in allTracksLyricsForGraph:
+	for name, values in listOfTracks.items():
+		if values["track_id"] == key:
+			songName = name
 	data = allTracksLyricsForGraph[key]["message"]["body"]["lyrics"]["lyrics_body"]
 	lyrics, disclaimer = data.split("...\n\n*******")
 	while "(" in lyrics and ")" in lyrics:
@@ -116,13 +121,45 @@ for key in allTracksLyricsForGraph:
 	lyrics=lyrics.replace(",","")
 	lyrics=lyrics.replace("!","")
 	words=lyrics.split()
-	for name, values in listOfTracks.items():
-		if values["track_id"] == key:
-			songName = name
 	for i in range(len(words)-1):
 		createNodes(words[i], words[i+1])
+	print(songName+" lyrics added to the graph")
+	paradigmaticQuery='''
+	MATCH (s:lyrics)
+	// Get right1, left1
+	MATCH (w:lyrics)-[:next]->(s)
+	WITH collect(DISTINCT w.word) as left1, s
+	MATCH (w:lyrics)<-[:next]-(s)
+	WITH left1, s, collect(DISTINCT w.word) as right1
+	// Match every other word
+	MATCH (o:lyrics) WHERE NOT s = o
+	WITH left1, right1, s, o
+	// Get other right, other left1
+	MATCH (w:lyrics)-[:next]->(o)
+	WITH collect(DISTINCT w.word) as left1_o, s, o, right1, left1
+	MATCH (w:lyrics)<-[:next]-(o)
+	WITH left1_o, s, o, right1, left1, collect(DISTINCT w.word) as right1_o
+	// compute right1 union, intersect
+	WITH FILTER(x IN right1 WHERE x IN right1_o) as r1_intersect,
+	  (right1 + right1_o) AS r1_union, s, o, right1, left1, right1_o, left1_o
+	// compute left1 union, intersect
+	WITH FILTER(x IN left1 WHERE x IN left1_o) as l1_intersect,
+	  (left1 + left1_o) AS l1_union, r1_intersect, r1_union, s, o
+	WITH DISTINCT r1_union as r1_union, l1_union as l1_union, r1_intersect, l1_intersect, s, o
+	WITH 1.0*length(r1_intersect) / length(r1_union) as r1_jaccard,
+	  1.0*length(l1_intersect) / length(l1_union) as l1_jaccard,
+	  s, o
+	WITH s, o, r1_jaccard, l1_jaccard, r1_jaccard + l1_jaccard as sim
+	CREATE UNIQUE (s)-[r:RELATED_TO]->(o) SET r.paradig = sim;
+	'''
+	with graph._driver.session() as session:
+	    session.run(paradigmaticQuery,songName=songName)
+	paradigmaticQueryResponse='''MATCH (s:lyrics)-[r:RELATED_TO]->(o:lyrics) RETURN s.word,o.word,r.paradig AS sim ORDER BY sim DESC;'''
+	with graph._driver.session() as session:
+	    weightedKeywords=session.run(paradigmaticQueryResponse)
 	with graph._driver.session() as session:
 		session.run(addSongNameAsLabel,songName=songName)
 	with graph._driver.session() as session:
 		session.run(removeLyricsLabel)
-	
+	print(songName+" MINED FOR PARADIGMATIC SIMILARITY USING JACCARD INDEX")
+	print(weightedKeywords)
