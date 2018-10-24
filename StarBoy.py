9c1cd4721dbfa9fde551eef9a12ef6b39e07fef2
Devansh Trivedi <devanshrtrivedi@gmail.com>
2018-10-24 12:30:35 +0530

    Day 36 update
    Improved Lyrics cleaning using loops
    CSV export is tip top, no blank lines now

--- StarBoy.py
+++ StarBoy.py
@@ -21,6 +21,7 @@ class Graph(object):
 			FIELDTERMINATOR ' '
 			FOREACH (w IN RANGE(0, SIZE(line)-2) | 
 			MERGE (lx:lyrics{word:line[w]})
+				ON CREATE SET lx.word = w
 				ON CREATE SET lx.count = 1
 				ON MATCH SET lx.count = lx.count + 1
 			MERGE (mx:lyrics{word:line[w+1]})
@@ -105,49 +106,54 @@ def create_Nodes():
 fetchedLyrics = get_Lyrics_From_Track_ID(115237681)
 lyrics, disclaimer = fetchedLyrics["message"]["body"]["lyrics"]["lyrics_body"].split("...\n\n*******")
 
-while "(" in lyrics and ")" in lyrics:
+while ("(" in lyrics and ")" in lyrics):
 	before, rest = lyrics.split("(",maxsplit=1)
 	inside, after = rest.split(")",maxsplit=1)
 	lyrics = before + after
-lyrics = lyrics.replace("\n\n","\n").replace(",","").replace("!","") #.replace("\n"," ")
+
+while ("\n\n" in lyrics):
+	lyrics = lyrics.replace("\n\n","\n")
+
+lyrics = lyrics.replace(",","").replace("!","")
+#.replace("\n"," ")
 lyricsLine = lyrics.split("\n")
 import csv
-with open (r'starboyExportedLyrics.csv', 'wb') as write_file:
+with open (r'starboyExportedLyrics.csv', 'w') as write_file:
 	write=csv.writer(write_file)
 	write.writerows([r] for r in lyricsLine)
 
 print("Starboy lyrics added to the graph")
 
-
-paradigmatic_Query='''
-// Mining Paradigmatic Word Associations using Jaccard Index to compute similarity
-MATCH (s:lyrics)
-// Get right1, left1
-MATCH (w:lyrics)-[:next]->(s)
-WITH collect(DISTINCT w.word) as left1, s
-MATCH (w:lyrics)<-[:next]-(s)
-WITH left1, s, collect(DISTINCT w.word) as right1
-// Match every other word
-MATCH (o:lyrics) WHERE NOT s = o
-WITH left1, right1, s, o
-// Get other right, other left1
-MATCH (w:lyrics)-[:next]->(o)
-WITH collect(DISTINCT w.word) as left1_o, s, o, right1, left1
-MATCH (w:lyrics)<-[:next]-(o)
-WITH left1_o, s, o, right1, left1, collect(DISTINCT w.word) as right1_o
-// compute right1 union, intersect
-WITH FILTER(x IN right1 WHERE x IN right1_o) as r1_intersect,
-  (right1 + right1_o) AS r1_union, s, o, right1, left1, right1_o, left1_o
-// compute left1 union, intersect
-WITH FILTER(x IN left1 WHERE x IN left1_o) as l1_intersect,
-  (left1 + left1_o) AS l1_union, r1_intersect, r1_union, s, o
-WITH DISTINCT r1_union as r1_union, l1_union as l1_union, r1_intersect, l1_intersect, s, o
-WITH 1.0*length(r1_intersect) / length(r1_union) as r1_jaccard,
-  1.0*length(l1_intersect) / length(l1_union) as l1_jaccard,
-  s, o
-WITH s, o, r1_jaccard, l1_jaccard, r1_jaccard + l1_jaccard as sim
-CREATE UNIQUE (s)-[r:RELATED_TO]->(o) SET r.paradig = sim;
-'''
+create_Nodes()
+# paradigmatic_Query='''
+# // Mining Paradigmatic Word Associations using Jaccard Index to compute similarity
+# MATCH (s:lyrics)
+# // Get right1, left1
+# MATCH (w:lyrics)-[:next]->(s)
+# WITH collect(DISTINCT w.word) as left1, s
+# MATCH (w:lyrics)<-[:next]-(s)
+# WITH left1, s, collect(DISTINCT w.word) as right1
+# // Match every other word
+# MATCH (o:lyrics) WHERE NOT s = o
+# WITH left1, right1, s, o
+# // Get other right, other left1
+# MATCH (w:lyrics)-[:next]->(o)
+# WITH collect(DISTINCT w.word) as left1_o, s, o, right1, left1
+# MATCH (w:lyrics)<-[:next]-(o)
+# WITH left1_o, s, o, right1, left1, collect(DISTINCT w.word) as right1_o
+# // compute right1 union, intersect
+# WITH FILTER(x IN right1 WHERE x IN right1_o) as r1_intersect,
+#   (right1 + right1_o) AS r1_union, s, o, right1, left1, right1_o, left1_o
+# // compute left1 union, intersect
+# WITH FILTER(x IN left1 WHERE x IN left1_o) as l1_intersect,
+#   (left1 + left1_o) AS l1_union, r1_intersect, r1_union, s, o
+# WITH DISTINCT r1_union as r1_union, l1_union as l1_union, r1_intersect, l1_intersect, s, o
+# WITH 1.0*length(r1_intersect) / length(r1_union) as r1_jaccard,
+#   1.0*length(l1_intersect) / length(l1_union) as l1_jaccard,
+#   s, o
+# WITH s, o, r1_jaccard, l1_jaccard, r1_jaccard + l1_jaccard as sim
+# CREATE UNIQUE (s)-[r:RELATED_TO]->(o) SET r.paradig = sim;
+# '''
 
 # paradigmatic_Query_Response='''MATCH (s:lyrics)-[r:RELATED_TO]->(o:lyrics) RETURN s.word,o.word,r.paradig ORDER BY r.paradig DESC;'''
 # def paradigmatic_Query_Response_Function(tx):
@@ -156,7 +162,7 @@ CREATE UNIQUE (s)-[r:RELATED_TO]->(o) SET r.paradig = sim;
 # 		list_Of_Tracks[song_Name]["jaccard"].append({tuple([record["s.word"], record["o.word"]]): record["r.paradig"]})
 
 # with graph._driver.session() as session:
-	 # session.run(paradigmatic_Query)
+# 	 session.run(paradigmatic_Query)
 # with graph._driver.session() as session:
 # 	 session.read_transaction(paradigmatic_Query_Response_Function)
 
