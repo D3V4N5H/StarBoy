d5547344d5f22137ff7f802126e06aa430de2abf
Devansh Trivedi <devanshrtrivedi@gmail.com>
2018-10-27 18:44:39 +0530

    Day 39 update
    Replicate the successful mining in Starboy in the loop of 'Mining Top Tracks in india' script

--- Mining Top Tracks in india.py
+++ Mining Top Tracks in india.py
@@ -4,6 +4,10 @@ uri = "bolt://localhost:7687"
 user = "neo4j"
 password = "password"
 
+import csv
+import os.path
+save_path = '/Users/d3v4n5h/Library/Application Support/Neo4j Desktop/Application/neo4jDatabases/database-25eef966-4d97-4421-9310-903f03421892/installation-3.4.6/import/'
+
 import json, requests
 def call_API(method, parameters):
     response = requests.get( "https://api.musixmatch.com/ws/1.1/"+ method + "?format=jsonp&callback=callback" + parameters + "&apikey=" + api_key ).text
@@ -42,9 +46,62 @@ remove_Lyrics_Label='''
 	RETURN node
 	'''
 
-def create_Nodes(lyricsWord1, lyricsWord2):
+paradigmatic_Query='''
+MATCH (s:lyrics)
+// Get right1, left1
+MATCH (w:lyrics)-[:next]->(s)
+WITH collect(DISTINCT w.word) as left1, s
+MATCH (w:lyrics)<-[:next]-(s)
+WITH left1, s, collect(DISTINCT w.word) as right1
+// Match every other word
+MATCH (o:lyrics) WHERE NOT s = o
+WITH left1, right1, s, o
+// Get other right, other left1
+MATCH (w:lyrics)-[:next]->(o)
+WITH collect(DISTINCT w.word) as left1_o, s, o, right1, left1
+MATCH (w:lyrics)<-[:next]-(o)
+WITH left1_o, s, o, right1, left1, collect(DISTINCT w.word) as right1_o
+// compute right1 union, intersect
+WITH FILTER(x IN right1 WHERE x IN right1_o) as r1_intersect,
+  (right1 + right1_o) AS r1_union, s, o, right1, left1, right1_o, left1_o
+// compute left1 union, intersect
+WITH FILTER(x IN left1 WHERE x IN left1_o) as l1_intersect,
+  (left1 + left1_o) AS l1_union, r1_intersect, r1_union, s, o
+WITH DISTINCT r1_union as r1_union, l1_union as l1_union, r1_intersect, l1_intersect, s, o
+WITH 1.0*length(r1_intersect) / length(r1_union) as r1_jaccard,
+  1.0*length(l1_intersect) / length(l1_union) as l1_jaccard,
+  s, o
+WITH s, o, r1_jaccard, l1_jaccard, r1_jaccard + l1_jaccard as sim
+CREATE UNIQUE (s)-[r:RELATED_TO]->(o) SET r.paradig = sim;
+'''
+
+def create_Nodes_Query_Fn(song_Name):
+	return '''LOAD CSV FROM "file:///''' + song_Name + '''.csv" AS line
+				FIELDTERMINATOR ' '
+				FOREACH (w IN RANGE(0, SIZE(line)-2) | 
+				MERGE (lx:lyrics{word:line[w]})
+					ON CREATE SET lx.word = w
+					ON CREATE SET lx.count = 1
+					ON MATCH SET lx.count = lx.count + 1
+				MERGE (mx:lyrics{word:line[w+1]})
+					ON CREATE SET mx.count = 1
+					ON MATCH SET mx.count = mx.count + (case when w = SIZE(line)-2 then 1 else 0 end)
+				MERGE (lx)-[r:next]->(mx)
+					ON CREATE SET r.count = 1
+					ON MATCH SET r.count = r.count +1)
+				RETURN line
+				'''
+
+def create_Nodes(create_Nodes_Query):
         with graph._driver.session() as session:
-            session.write_transaction(graph.create_Nodes, lyricsWord1, lyricsWord2)
+            session.write_transaction(graph.create_Nodes, create_Nodes_Query)
+
+paradigmatic_Query_Response='''MATCH (s:lyrics)-[r:RELATED_TO]->(o:lyrics) RETURN s.word,o.word,r.paradig ORDER BY r.paradig DESC;'''
+def paradigmatic_Query_Response_Function(tx):
+	for record in tx.run(paradigmatic_Query_Response):
+		# print(record["s.word"], record["o.word"], record["sim"])
+		list_Of_Tracks[song_Name]["jaccard"].append({tuple([record["s.word"], record["o.word"]]): record["r.paradig"]})
+
 
 class Graph(object):
     def __init__(self, uri, user, password):
@@ -57,22 +114,8 @@ class Graph(object):
             return session.run("MATCH (n)"
                                "DETACH DELETE n")
     @staticmethod
-    def create_Nodes(tx, lyricsWord1, lyricsWord2):
-        result = tx.run('''
-        	UNWIND $lyricsWord1 AS word1
-        	UNWIND $lyricsWord2 AS word2
-            MERGE (lx:lyrics{word:word1})
-            	ON CREATE SET lx.word = word1
-                ON CREATE SET lx.count = 1
-                ON MATCH SET lx.count = lx.count + 1
-            MERGE (mx:lyrics{word:word2})
-                ON CREATE SET mx.word = word2
-                ON CREATE SET mx.count = 1
-                ON MATCH SET mx.count = mx.count + 1
-            MERGE (lx)-[r:next]->(mx)
-                ON CREATE SET r.count = 1
-                ON MATCH SET r.count = r.count +1
-            RETURN word1, word2''', lyricsWord1=lyricsWord1, lyricsWord2=lyricsWord2)
+    def create_Nodes(tx, create_Nodes_Query):
+        result = tx.run(create_Nodes_Query)
         return result
 
 
@@ -100,54 +143,31 @@ for track_Dictionary in top_Tracks_India_Data["message"]["body"]["track_list"]:
     for dictionary in track_Dictionary.items():
         all_Tracks_Lyrics_For_Graph[dictionary[1]["track_id"]] = get_Lyrics_From_Track_ID(dictionary[1]["track_id"])
 
-paradigmatic_Query_Response='''MATCH (s:lyrics)-[r:RELATED_TO]->(o:lyrics) RETURN s.word,o.word,r.paradig AS sim ORDER BY sim DESC;'''
-def paradigmatic_Query_Response_Function(tx):
-	for record in tx.run(paradigmatic_Query_Response):
-		# print(record["s.word"], record["o.word"], record["sim"])
-		list_Of_Tracks[song_Name]["jaccard"].append({tuple([record["s.word"], record["o.word"]]): record["sim"]})
 
 for key in all_Tracks_Lyrics_For_Graph:
 	for name, values in list_Of_Tracks.items():
 		if values["track_id"] == key:
 			song_Name = name
-	data = all_Tracks_Lyrics_For_Graph[key]["message"]["body"]["lyrics"]["lyrics_body"]
-	lyrics, disclaimer = data.split("...\n\n*******")
-	while "(" in lyrics and ")" in lyrics:
+	fetchedLyrics = all_Tracks_Lyrics_For_Graph[key]["message"]["body"]["lyrics"]["lyrics_body"]
+	lyrics, disclaimer = fetchedLyrics.split("...\n\n*******")
+	while ("(" in lyrics and ")" in lyrics):
 		before, rest = lyrics.split("(",maxsplit=1)
 		inside, after = rest.split(")",maxsplit=1)
 		lyrics = before + after
-	lyrics=lyrics.replace("\n\n","\n").replace("\n"," ").replace(",","").replace("!","")
-	words=lyrics.split()
-	create_Nodes(words[1:], words[:-1])
+	
+	while ("\n\n" in lyrics):
+		lyrics = lyrics.replace("\n\n","\n")
+	
+	lyrics=lyrics.replace(",","").replace("!","")
+	lyricsLine = lyrics.split("\n")
+	completeName = os.path.join(save_path, song_Name + ".csv")
+	with open (completeName, 'w') as write_file:
+		write=csv.writer(write_file)
+		write.writerows([r] for r in lyricsLine)
+	create_Nodes_Query = create_Nodes_Query_Fn(song_Name)
+	create_Nodes(create_Nodes_Query)
 	print(song_Name+" lyrics added to the graph")
-	paradigmatic_Query='''
-	MATCH (s:lyrics)
-	// Get right1, left1
-	MATCH (w:lyrics)-[:next]->(s)
-	WITH collect(DISTINCT w.word) as left1, s
-	MATCH (w:lyrics)<-[:next]-(s)
-	WITH left1, s, collect(DISTINCT w.word) as right1
-	// Match every other word
-	MATCH (o:lyrics) WHERE NOT s = o
-	WITH left1, right1, s, o
-	// Get other right, other left1
-	MATCH (w:lyrics)-[:next]->(o)
-	WITH collect(DISTINCT w.word) as left1_o, s, o, right1, left1
-	MATCH (w:lyrics)<-[:next]-(o)
-	WITH left1_o, s, o, right1, left1, collect(DISTINCT w.word) as right1_o
-	// compute right1 union, intersect
-	WITH FILTER(x IN right1 WHERE x IN right1_o) as r1_intersect,
-	  (right1 + right1_o) AS r1_union, s, o, right1, left1, right1_o, left1_o
-	// compute left1 union, intersect
-	WITH FILTER(x IN left1 WHERE x IN left1_o) as l1_intersect,
-	  (left1 + left1_o) AS l1_union, r1_intersect, r1_union, s, o
-	WITH DISTINCT r1_union as r1_union, l1_union as l1_union, r1_intersect, l1_intersect, s, o
-	WITH 1.0*length(r1_intersect) / length(r1_union) as r1_jaccard,
-	  1.0*length(l1_intersect) / length(l1_union) as l1_jaccard,
-	  s, o
-	WITH s, o, r1_jaccard, l1_jaccard, r1_jaccard + l1_jaccard as sim
-	CREATE UNIQUE (s)-[r:RELATED_TO]->(o) SET r.paradig = sim;
-	'''
+	
 	with graph._driver.session() as session:
 	    session.run(paradigmatic_Query,song_Name=song_Name)
 	with graph._driver.session() as session:
@@ -156,4 +176,6 @@ for key in all_Tracks_Lyrics_For_Graph:
 		session.run(add_Song_Name_As_Label,song_Name=song_Name)
 	with graph._driver.session() as session:
 		session.run(remove_Lyrics_Label)
-	print(song_Name+" MINED FOR PARADIGMATIC SIMILARITY USING JACCARD INDEX")
+	print(song_Name+" MINED FOR PARADIGMATIC ASSOCIATIONS USING JACCARD INDEX")
+
+graph.close()
