9a5a23153fbed7f27a61283bbefaecec2959dec5
Devansh Trivedi <devanshrtrivedi@gmail.com>
2018-11-17 00:13:00 +0530

    Day 59 update
    Update deprecated Neo4j driver
    Use the config file for credentials

--- SongRecommendation.py
+++ SongRecommendation.py
@@ -1,8 +1,11 @@
-from neo4j.v1 import GraphDatabase
+import configparser
+config = configparser.ConfigParser()
+config.read('config.txt')
 
-uri = "bolt://localhost:7687"
-user = "neo4j"
-password = "password"
+from neo4j import GraphDatabase
+uri = config['Neo4j']['Bolt_URI']
+user = config['Neo4j']['User']
+password = config['Neo4j']['Password']
 
 class Graph(object):
     def __init__(self, uri, user, password):
