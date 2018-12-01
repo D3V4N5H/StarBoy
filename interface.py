1b5ad3302d2e0c211752c0bdf8a4eb11107d6de7
Devansh Trivedi <devanshrtrivedi@gmail.com>
2018-12-01 20:27:01 +0530

    Day 74 update
    Using TKinter 'Pack' Geometry Management Method to display text on Graphical User Interface

---
+++ interface.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter
+import _tkinter
+
+tkinter._test()
+
+root = Tk()
+title = ttk.Label(root, text= 'Flubber - Your Music Connoisseur', justify= CENTER)
+title.pack()
+
+root.mainloop()
