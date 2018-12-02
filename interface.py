3639450e4dbf8b2447a6adcf0bd8b869d2fa47fd
Devansh Trivedi <devanshrtrivedi@gmail.com>
2018-12-02 18:45:04 +0530

    Day 75 update
    Display image on GUI using Python 3 PhotoImage object

--- interface.py
+++ interface.py
@@ -2,11 +2,17 @@ from tkinter import *
 from tkinter import ttk
 import tkinter
 import _tkinter
-
-tkinter._test()
-
+# tkinter._test()
 root = Tk()
+
 title = ttk.Label(root, text= 'Flubber - Your Music Connoisseur', justify= CENTER)
 title.pack()
+
+bio = ttk.Label(root, text= 'Havana', wraplength= 150)
+bio.pack()
+
+coverFile = PhotoImage(file= 'havana.gif')
+coverArt = Label(root, image= coverFile)
+coverArt.pack()
 
 root.mainloop()
